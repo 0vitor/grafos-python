f50049fe408d1eefbfce02e04a3a2a54c854761a
vitor <w.wutaes@gmail.com>
2023-06-13 09:05:16 -0300

    fix: fix both syntax and declarations errors

--- projetoGrafos.py
+++ projetoGrafos.py
@@ -4,77 +4,73 @@ class Vertice:
         self.vizinhos = []
 
 class Aresta:
-    def __init__(vertice1, vertice2):
+    def __init__(self, vertice1, vertice2):
         self.vertice1 = vertice1
         self.vertice2 = vertice2
 
 class GrafroMatriz:
-    def __init__(self, tamanho)
+    def __init__(self, tamanho):
         self.tamanhoMax = tamanho
-        self.linha = [v1,v2,v3]
+        self.linha = []
         self.coluna = []
         self.matriz = []
 
     def __inicarMatriz(self):
         coluna = []
-        for _ in range(tamanho):
+        for _ in range(self.tamanho):
                 coluna.append(0)
 
-        for _ in range(tamanho):
-            matriz.append(coluna)
-
-    def addVertice():
+        for _ in range(self.tamanho):
+            self.matriz.append(coluna)
 
     def criarColuna(self):
-        for _ in range(tamanho):
-            for _ in range(tamanho):
-                coluna.append(vertice)
+        for _ in range(self.tamanho):
+            for _ in range(self.tamanho):
+                self.coluna.append(self.vertice)
 
-            matriz.append(coluna)
+            self.matriz.append(self.coluna)
 
-    def adicionarArestas(aresta):
-        matriz[aresta.vertice1.index][aresta.vertice2.index] += 1
-        matriz[aresta.vertice2.index][aresta.vertice1.index] += 1
+    def adicionarArestas(self, aresta):
+        self.matriz[aresta.vertice1.index][aresta.vertice2.index] += 1
+        self.matriz[aresta.vertice2.index][aresta.vertice1.index] += 1
 
-    def removerArestas(aresta):
-        endeco = matriz[aresta.vertice1.index][aresta.vertice2.index]
-        if(endeco)
-            matriz[aresta.vertice1.index][aresta.vertice2.index] -= 1
-            matriz[aresta.vertice2.index][aresta.vertice1.index] -= 1
+    def removerArestas(self, aresta):
+        endeco = self.matriz[aresta.vertice1.index][aresta.vertice2.index]
+        if(endeco):
+            self.matriz[aresta.vertice1.index][aresta.vertice2.index] -= 1
+            self.matriz[aresta.vertice2.index][aresta.vertice1.index] -= 1
 
-    def calcularGrauVertice(verticeIndex):
-        linha = matriz[verticeIndex]
+    def calcularGrauVertice(self, verticeIndex):
+        linha = self.matriz[verticeIndex]
         contadorDeVertices = 0
         for valor in linha:
             contadorDeVertices += valor
 
         return contadorDeVertices
 
-    def calcularGrau():
+    """ def calcularGrau(self):
         contadorDeVertices = 0
-        for _ in range(tamanho):
-            calcularGrauVertice()
+        for _ in range(self.tamanho):
+            self.calcularGrauVertice()
             contadorDeVertices += valor
+        return contadorDeVertices """
 
-
-        return contadorDeVertices
-
-    def eVizinho(vertice1, vertice2):
-        endeco = matriz[vertice1.index][vertice2.index]
+    def eVizinho(self, vertice1, vertice2):
+        endeco = self.matriz[vertice1.index][vertice2.index]
         if(endeco):
             return True
         return False
 
-    def imprimirGrafo():
+    """ def imprimirGrafo():
+        print(self.tamanho)
+ """
 
-        print(tamanho)
-        print()
-
-    def criarLigacao(v1, v2):
-
+"""     def criarLigacao(v1, v2):
+ """
 def main():
+  return
 
-class Client:
+""" class Client:
     def criarGrafo(tipoDeEstrutura, tamanho):
         if(tipoDeEstrutura == 'EA'):
             instanciarEA(tamanho)
@@ -89,4 +85,4 @@ class Client:
         return GrafroMatriz()
 
     def instanciarMA(tamanho):
-        return estruturaMatriz()
+        return estruturaMatriz() """
