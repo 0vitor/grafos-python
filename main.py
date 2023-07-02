23da84cea3b613c7bdcb8fca816d264278810611
Pedro Isidorio <95106197+PedroHIS@users.noreply.github.com>
2023-07-01 22:50:03 -0300

    Modificações decorrentes da adição da classe Grafo

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-from grafosEA import GraphDrawerEA
-from grafosMA import GraphDrawerMA
-from exemplos import exemploGrafo1MA, exemploGrafo1EA, exemploGrafo2MA, exemploGrafo2EA
+from exemplos import exemploGrafo1MA, exemploGrafo1EA, exemploGrafo2MA, exemploGrafo2EA, testeMatriz, testeEstrutura
 
 def main():
+  
   grafoEx1MA = exemploGrafo1MA()
   grafoEx2MA = exemploGrafo2MA()
   grafoEx1EA = exemploGrafo1EA()
   grafoEx2EA = exemploGrafo2EA()
+  
+  #grafoEx1MA.desenharGrafo()
+  #grafoEx2MA.desenharGrafo()
+  #grafoEx1EA.desenharGrafo()
+  #grafoEx2EA.desenharGrafo()
 
-  #GraphDrawerMA(grafoEx1MA.matriz)
-  #GraphDrawerMA(grafoEx2MA.matriz)
-  #GraphDrawerEA(grafoEx1EA.estrutura)
-  #GraphDrawerEA(grafoEx2EA.estrutura)
+  #testeMatriz()
+  #testeEstrutura()
 
 main()
